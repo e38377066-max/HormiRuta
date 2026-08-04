03ab431da99eb78bc07b7d7b710c444f5f218cdc
Manuel Lezkano <e38377066@gmail.com>
2026-08-04 17:37:25 +0000

    Add compiled dist/ bundle with Google Maps key hardcoded as fallback

---
+++ dist/assets/index-DEIL-99Q.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CcO3rFjP.js","assets/vendor-capacitor-MtSxMQXH.js"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./vendor-capacitor-MtSxMQXH.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-CcO3rFjP.js"),__vite__mapDeps([0,1])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
